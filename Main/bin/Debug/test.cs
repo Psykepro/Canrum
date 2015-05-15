56101e347a02351b347c73cb28373d3df8ef6b6e
Bonnev <piggov@gmail.com>
2015-05-15 19:05:33 +0300

    Compile and execution of file test

---
+++ Main/bin/Debug/test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main2
+{
+    class Main1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[args.Length-1]);
+            Console.ReadKey();
+        }
+    }
+}
