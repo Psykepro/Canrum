7c721ae7fd97e51929c69071730d1d85808fe68b
pataroka <predjov@yahoo.com>
2015-05-20 17:36:16 +0300

    started game.cs

--- Main/Game.cs
+++ Main/Game.cs
@@ -1,13 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Main
 {
-    class Game
+    static class Game
     {
+        private static List<Player> Players = new List<Player>();
 
+        static Game()
+        {
+            StartScreen();
+            InitGame();
+            Tick();
+
+
+        }
+
+
+        public static void StartScreen()
+        {
+            
+        }
+
+
+        public static void InitGame()
+        {
+            Console.Write("Number of players: ");
+            int numberOfPlayers = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                Players.Add(new Player());
+            }
+        }
+
+        public static void Tick()
+        {
+            bool win = false;
+
+            while (win == false)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    foreach (var player in Players)
+                    {
+                        Console.WriteLine(player.Name + "'s turn.");
+                        Console.WriteLine(player.Name + "meets" player.Boss[0].Name);
+                        player.StartCodeCompiler();
+
+                    }
+                }
+            }
+        }
+
+
+        
     }
 }
