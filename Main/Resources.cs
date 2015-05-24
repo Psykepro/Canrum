b6002e06dbcf6a3ce77ea189d029604d3e3d0449
GeorgiDinev <georgi.dinevv@gmail.com>
2015-05-24 18:31:32 +0300

    Reduced the number of Libraries

--- Main/Resources.cs
+++ Main/Resources.cs
@@ -7,45 +7,15 @@ namespace Main
     {
         private static readonly string[] Libraries =
         {
-            "System.Activities",
-            "System.AddIn",
-            "System.CodeDom",
             "System.Collections.Generic",
-            "System.ComponentModel",
-            "System.Configuration",
-            "System.Data",
-            "System.Deployment",
-            "System.Device.Location",
-            "System.Diagnostics",
-            "System.DirectoryServices",
-            "System.Dynamic",
-            "System.EnterpriseServices",
             "System.Globalization",
-            "System.IdentityModel",
             "System.IO",
             "System.Linq",
-            "System.Management",
             "System.Media",
-            "System.Messaging",
-            "System.Net",
             "System.Numerics",
-            "System.Printing",
-            "System.Reflection",
-            "System.Resources",
-            "System.Runtime",
-            "System.Security",
-            "System.ServiceModel",
-            "System.ServiceProcess",
-            "System.Speech",
-            "System.Text",
+            "System.Text.RegularExpressions;",
             "System.Threading",
-            "System.Threading.Tasks.Dataflow",
-            "System.Timers",
-            "System.Transactions",
-            "System.Web",
-            "System.Windows",
-            "System.Workflow",
-            "System.Xaml"
+            "System.Timers"
         };
 
         private static readonly string[] BossNames =
