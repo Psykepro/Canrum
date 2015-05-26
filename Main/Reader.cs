8a6ee68d8f9b855103fa382dc671b0b3136f64d2
Bonnev <piggov@gmail.com>
2015-05-26 23:00:31 +0300

    Changed encoding of all missions to Unicode

--- Main/Reader.cs
+++ Main/Reader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Main
 {
@@ -11,16 +12,15 @@ namespace Main
 
             try
             {
-                using (StreamReader sr = new StreamReader(filePath))
+                using (StreamReader sr = new StreamReader(filePath, Encoding.Unicode))
                 {
 
                     string readTheFile = sr.ReadToEnd();
-                    string[] dataParts = readTheFile.Split(new[] {'⑱'}, StringSplitOptions.RemoveEmptyEntries);
+                    string[] dataParts = readTheFile.Split(new[] { '§' }, StringSplitOptions.RemoveEmptyEntries);
                     data = new string[3][];
                     data[0] = new[] {dataParts[0]};
                     data[1] = dataParts[1].Split('Ẋ');
                     data[2] = dataParts[2].Split('Ẋ');
-
                 }
             }
             catch (FileNotFoundException)
